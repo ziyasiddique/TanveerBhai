48929044758d53b3c20466b68b8ee27f128187b3
Ziya Siddiqui <89196611+ziyasiddique@users.noreply.github.com>
2024-03-21 19:25:57 +0530

    CODE FILES AND IMAGES ARE IN DIFFERENT FOLDER

---
+++ TanveerBhai/new_folder/src/pages/components/case-studies.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function CaseStudy() {
+    return (
+        <div className='case-study-wrapper'>
+            <h2>Case Studies</h2>
+
+            <div className="case-study-container">
+                <div className="img">
+                    <img src="" alt="" />
+                </div>
+                <div className="content">
+                    <h2>COVID-19 Vaccination Protocol for India</h2>
+
+                    <p>This program architecture provides a holistic perspective for operationalising vaccination across four distinct phases while optimising for transparency, efficiency and effectiveness.</p>
+
+                    <a href="">{`Read More >`}</a>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default CaseStudy
---
+++ TanveerBhai/new_folder/src/pages/index.js
@@ -0,0 +1,53 @@
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return (
+    <>
+
+
+      {/* header section */}
+      <div className="header">
+
+      </div>
+      {/* banner section */}
+      <div className="banner-section">
+        <div className="background-image">
+          <div className="container hero-wrapper">
+            <div className="text-container">
+              <h2>AI</h2>
+              <h3>for good</h3>
+              <div className="para-content">
+                <p>At Fractal we harness the power of AI to
+                  <br></br>
+                  create positive impact in the world
+                </p>
+              </div>
+            </div>
+
+            <div className="image-wrapper">
+
+              <img src="/Group-163219-scaled-1.webp" alt="group-scale" />
+            </div>
+          </div>
+
+        </div>
+      </div >
+      {/* curve ball */}
+
+      {/* mapp section */}
+
+      {/* stories */}
+
+
+
+      {/* case studies */}
+
+
+      {/* form */}
+
+      {/* footer */}
+    </>
+  );
+}
